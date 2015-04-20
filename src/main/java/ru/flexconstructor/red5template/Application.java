9886028106b5e4270e677c5da7fcd6421059c3d3
flexconstructor <flexconstructor@gmail.com>
2015-04-20 11:25:05 +0300

    V-2 created spring security project

--- src/main/java/ru/flexconstructor/red5template/Application.java
+++ src/main/java/ru/flexconstructor/red5template/Application.java
@@ -5,8 +5,10 @@ import org.red5.server.adapter.MultiThreadedApplicationAdapter;
 import org.red5.server.api.scope.IScope;
 import org.slf4j.Logger;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * Main application entry point for the chat application.
@@ -16,9 +18,11 @@ import org.springframework.context.ApplicationContextAware;
 public class Application extends MultiThreadedApplicationAdapter implements ApplicationContextAware {
 
     private static Logger log = Red5LoggerFactory.getLogger(Application.class, "chat");
-
+    private UserDetailsService userDetailsService;
     @SuppressWarnings("unused")
     private ApplicationContext applicationContext;
+
+
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -39,4 +43,12 @@ public class Application extends MultiThreadedApplicationAdapter implements Appl
         super.appStop(scope);
     }
 
+    @Required
+    public UserDetailsService getUserDetailsService() {
+        return userDetailsService;
+    }
+
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 }
--- src/main/java/ru/flexconstructor/red5template/services/MyUserDetailsService.java
+++ src/main/java/ru/flexconstructor/red5template/services/MyUserDetailsService.java
@@ -1,7 +1,11 @@
 package ru.flexconstructor.red5template.services;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import ru.flexconstructor.red5template.dao.UserDao;
-import ru.flexconstructor.red5template.entity.User;
 import ru.flexconstructor.red5template.entity.UserRole;
 
 import java.util.ArrayList;
@@ -11,13 +15,13 @@ import java.util.Set;
 
 public class MyUserDetailsService implements UserDetailsService {
 
-	private UserDao userDao;
+    private UserDao userDao;
 
-	@Override
-	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
 
-		// Programmatic transaction management
-		/*
+        // Programmatic transaction management
+        /*
 		return transactionTemplate.execute(new TransactionCallback<UserDetails>() {
 
 			public UserDetails doInTransaction(TransactionStatus status) {
@@ -28,41 +32,41 @@ public class MyUserDetailsService implements UserDetailsService {
 			}
 
 		});*/
-		
-	    User user = userDao.findByUserName(username);
-		List<GrantedAuthority> authorities = buildUserAuthority(user.getUserRole());
 
-		return buildUserForAuthentication(user, authorities);
-		
+        ru.flexconstructor.red5template.entity.User user = userDao.findByUserName(username);
+        List<GrantedAuthority> authorities = buildUserAuthority(user.getUserRole());
 
-	}
+        return buildUserForAuthentication(user, authorities);
 
-	// Converts com.mkyong.users.model.User user to
-	// org.springframework.security.core.userdetails.User
-	private User buildUserForAuthentication(com.mkyong.users.model.User user, List<GrantedAuthority> authorities) {
-		return new User(user.getUsername(), user.getPassword(), user.isEnabled(), true, true, true, authorities);
-	}
 
-	private List<GrantedAuthority> buildUserAuthority(Set<UserRole> userRoles) {
+    }
 
-		Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
+    // ru.flexconstructor.red5template.entity.Userl.User user to
+    // org.springframework.security.core.userdetails.User
+    private org.springframework.security.core.userdetails.User buildUserForAuthentication(ru.flexconstructor.red5template.entity.User user, List<GrantedAuthority> authorities) {
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.isEnabled(), true, true, true, authorities);
+    }
 
-		// Build user's authorities
-		for (UserRole userRole : userRoles) {
-			setAuths.add(new SimpleGrantedAuthority(userRole.getRole()));
-		}
+    private List<GrantedAuthority> buildUserAuthority(Set<UserRole> userRoles) {
 
-		List<GrantedAuthority> Result = new ArrayList<GrantedAuthority>(setAuths);
+        Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
 
-		return Result;
-	}
+        // Build user's authorities
+        for (UserRole userRole : userRoles) {
+            setAuths.add(new SimpleGrantedAuthority(userRole.getRole()));
+        }
 
-	public UserDao getUserDao() {
-		return userDao;
-	}
+        List<GrantedAuthority> Result = new ArrayList<GrantedAuthority>(setAuths);
 
-	public void setUserDao(UserDao userDao) {
-		this.userDao = userDao;
-	}
+        return Result;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
 }
